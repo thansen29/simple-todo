af75f8c0f268b6ae93e46a57ff0e8ba00ae7ff33
Thomas Hansen <thansen1694@gmail.com>
2018-04-12 17:27:12 -0400

    File structure and template built out

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-container/todo-list/todo-list.component';
+import { TodoContainerComponent } from './todo-container/todo-container.component';
+import { TaskViewComponent } from './todo-container/task-view/task-view.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    TodoListComponent,
+    TodoContainerComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule
